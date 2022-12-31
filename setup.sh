9f9feda3b50c88376c34a396a22ce74ff2070006
hasandramali <93338836+hasandramali@users.noreply.github.com>
2022-12-31 21:36:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ fi
 megadl "https://mega.nz/file/wb5DmYxZ#T0q1BdePVDLvS1dhVIuuUobMCx1XXffHrvLuBbt2l7g"
 tar -xf czeror-termux.tar
 rm czeror-termux.tar
-chmod +x Ubuntu-i386.sh
-./Ubuntu-i386.sh
+chmod +x czeror-termux/Ubuntu-i386.sh
+bash czeror-termux/Ubuntu-i386.sh
 clear
 if [ ! -d "/data/data/com.termux/files/home/storage/shared/xash" ]; then
 echo -e "\e[1;31m ERROR:\e[0m Please Copy xashds/czeror folder to your xash directory."
@@ -26,3 +26,4 @@ fi
 
 mv czeror-termux/xashds ubuntu-fs/root/
 echo -e "\033[0;36m Type ./start-ubuntu.sh\033[0m"
+rm -rf czeror-termux
